fb9965c394b21f99fbd271e896ee0ed2627a0c33
pinetree <bosollee@gmail.com>
2023-02-19 23:59:29 +0900

    feat: maniaDB 검색 Api 연동 및 proxy 세팅

--- src/App.js
+++ src/App.js
@@ -5,8 +5,44 @@ const { Configuration, OpenAIApi } = require('openai');
 function App() {
   const [music, setMusic] = useState({ title: '', singer: '' });
 
+  // Mania search API 호출
+  const fetchMusicSearchApi = useCallback(() => {
+    const { title, singer } = music;
+    const searchKeyword = 'Poker Face'; // FIXME: 추후 수정 예정
+
+    fetch(`/api/search/${searchKeyword}/?sr=song&display=10&v=0.5`)
+      .then((response) => response.text())
+      .then((str) => new window.DOMParser().parseFromString(str, 'text/xml'))
+      .then((data) => {
+        console.log(data);
+        const items = data.querySelectorAll('item');
+
+        let html = ``;
+        items.forEach((el) => {
+          html += `
+            <article>
+              <img src="${
+                el.querySelector('link').innerHTML
+              }/image/large.png" alt="">
+              <h2>
+                <a href="${
+                  el.querySelector('link').innerHTML
+                }" target="_blank" rel="noopener">
+                  ${el.querySelector('title').innerHTML}
+                </a>
+              </h2>
+            </article>
+          `;
+        });
+        document.body.insertAdjacentHTML('beforeend', html);
+      })
+      .catch((error) => {
+        console.log(`error: ${error}`);
+      });
+  }, []);
+
   // OpenAI API 호출
-  const fetchOpenApi = useCallback(() => {
+  const fetchOpenAIApi = useCallback(() => {
     const configuration = new Configuration({
       apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
@@ -29,7 +65,8 @@ function App() {
   }, []);
 
   useEffect(() => {
-    fetchOpenApi(); // Mount 시 호출한다.
+    fetchOpenAIApi(); // Mount 시 호출한다.
+    fetchMusicSearchApi();
   }, []);
 
   const { title, singer } = music;
